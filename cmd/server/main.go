c1dfae1622800e0365e7848ba96a1f1b4c4f3b9f
phumiphat <phumiphat.auk@gmail.com>
2025-05-07 15:40:57 +0700

    Add viper

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+
+	"go.learning/config"
 
 	echo "github.com/labstack/echo/v4"
 )
+
+var conf config.Config
+
+func init() {
+	var err error
+	conf, err = config.LoadConfig()
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%d", conf.Server.Port)))
 }
---
+++ config/config.go
@@ -0,0 +1,69 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Server ServerConfig `mapstructure:"server"`
+}
+
+type ServerConfig struct {
+	Port     uint   `mapstructure:"port"`
+	User     string `mapstructure:"user"`
+	Password string `mapstructure:"password"`
+}
+
+func LoadConfig() (config Config, err error) {
+	var c Config
+	viper.AddConfigPath("config")
+	viper.SetConfigName("config")
+	viper.SetConfigType("yml")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return c, err
+	}
+
+	viper.Unmarshal(&c)
+
+	c.Server = ServerConfig{
+		Port:     getEnvInteger("server.port", c.Server.Port),
+		User:     getEnv("server.user", c.Server.User),
+		Password: getEnv("server.password", c.Server.Password),
+	}
+
+	fmt.Printf("Port after %d\n", c.Server.Port)
+
+	return c, nil
+}
+
+func getEnv(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
+}
+
+func getEnvInteger(key string, defaultValue uint) uint {
+	if strValue := os.Getenv(key); strValue != "" {
+		var intValue uint
+		_, err := fmt.Sscan(strValue, &intValue)
+		if err == nil {
+			return intValue
+		}
+	}
+	return defaultValue
+}
+
+func getEnvBool(key string, defaultValue bool) bool {
+	if value := os.Getenv(key); value != "" {
+		return value == "true"
+	}
+	return defaultValue
+}
